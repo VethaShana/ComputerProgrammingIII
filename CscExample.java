f15b929903db259b7121e63d24a5b47fe8ce28e6
VethaShana <vvvttt479@gmail.com>
2020-09-18 04:52:23 +0530

    addingcsc

--- CscExample.java
+++ CscExample.java
@@ -2,4 +2,16 @@ Class CscExample{
 	int questionNo;
 	String category;
 	String text;
+	
+	public void addQuestion(CscExample qu){
+
+	}
+
+	public void removeQuestion(int quID){
+
+	}
+
+	public void editQuestion(int quID){
+
+	}
 }
