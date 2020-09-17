64885fae504d66c626fd704fd240182ad904f5b2
VethaShana <vvvttt479@gmail.com>
2020-09-18 04:48:47 +0530

    adding..

--- CscExample.java
+++ CscExample.java
@@ -2,4 +2,31 @@ Class CscExample{
 	int questionNo;
 	String category;
 	String text;
+
+	public void setQuestionNo(int q){
+		questionNo=q;
+	}
+
+	public void setCategory(String c){
+		category=c;
+	}
+
+	public void setText(String t){
+		text = t;
+	}
+
+	public int getQuestionNo(){
+		return questionNo;
+	}
+
+	public String getCategory(){
+		return category;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	
+	
 }
